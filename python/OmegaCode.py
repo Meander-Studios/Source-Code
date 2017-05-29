51663cb34898f5e192007b17145b89c2e80c7f2a
Geekcoder127 <awesome2723@oultook.com>
2017-05-29 19:31:59 +0100

    Improved OmegaCode Python

--- python/OmegaCode.py
+++ python/OmegaCode.py
@@ -4,3 +4,17 @@ def write(x):
             print x[i]
     else:
         print x
+def emailVerification(email):
+    email = email.lower()
+    if "@" in email:
+        email = email.split('@')
+        if "." in email[1]:
+            website = email[1].split('.')
+            if website[0]=='example':
+                return False
+            else:
+                return True
+        else:
+            return False
+    else:
+        return False
