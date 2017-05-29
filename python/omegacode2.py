a530b2bae828d2702d2c8e9b7497b0f147cc6bad
monsieurcochon <jacobmulliken+github@gmail.com>
2017-05-29 14:42:04 -0400

    Added DOME to omegacode

---
+++ python/omegacode2.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 29 14:11:00 2017
+
+@author: Coby Mulliken
+"""
+
+def encrypt(cipher, offset):
+    for x in range(97, 97 + 26):
+        cipher[chr(x)] = chr(((x + offset - 97) % 26) + 97)
+    cipher[' '] = chr(32)
+    return cipher
+
+def dome(offset1, offset2, string):
+    inpu = string.lower()
+    cipher = {}
+    output = ""
+    
+    print("Printing offset values...")
+    
+    for y in range(0,len(inpu)):
+        """ Concatenates outpu and encrypted outpu """
+        offset = offset2 * y + offset1 
+        print("run ", y, " offset is ", offset)
+        cipher = encrypt(cipher, offset)
+        output = output + cipher[inpu[y:y + 1]]
+    
+    print(output)
